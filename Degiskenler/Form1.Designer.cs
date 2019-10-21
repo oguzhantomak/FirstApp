19f5dbf17f095f0a958b73fcbb5a9bcb69f97f81
oguzhantomak <oguzhantomak@gmail.com>
2019-10-21 13:56:24 +0300

    Add project files.

---
+++ Degiskenler/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Degiskenler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pcbAvatar = new System.Windows.Forms.PictureBox();
+            this.lblKullaniciAdi = new System.Windows.Forms.Label();
+            this.lblSifre = new System.Windows.Forms.Label();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.lnkReSendPass = new System.Windows.Forms.LinkLabel();
+            this.lnkNewAcc = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAvatar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbAvatar
+            // 
+            this.pcbAvatar.Image = ((System.Drawing.Image)(resources.GetObject("pcbAvatar.Image")));
+            this.pcbAvatar.Location = new System.Drawing.Point(60, 12);
+            this.pcbAvatar.Name = "pcbAvatar";
+            this.pcbAvatar.Size = new System.Drawing.Size(183, 183);
+            this.pcbAvatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pcbAvatar.TabIndex = 0;
+            this.pcbAvatar.TabStop = false;
+            // 
+            // lblKullaniciAdi
+            // 
+            this.lblKullaniciAdi.AutoSize = true;
+            this.lblKullaniciAdi.Location = new System.Drawing.Point(57, 222);
+            this.lblKullaniciAdi.Name = "lblKullaniciAdi";
+            this.lblKullaniciAdi.Size = new System.Drawing.Size(73, 13);
+            this.lblKullaniciAdi.TabIndex = 1;
+            this.lblKullaniciAdi.Text = "Kullanıcı Adı : ";
+            // 
+            // lblSifre
+            // 
+            this.lblSifre.AutoSize = true;
+            this.lblSifre.Location = new System.Drawing.Point(93, 255);
+            this.lblSifre.Name = "lblSifre";
+            this.lblSifre.Size = new System.Drawing.Size(37, 13);
+            this.lblSifre.TabIndex = 2;
+            this.lblSifre.Text = "Şifre : ";
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(137, 222);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtKullaniciAdi.TabIndex = 3;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(137, 255);
+            this.txtSifre.MaxLength = 12;
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(100, 20);
+            this.txtSifre.TabIndex = 4;
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Location = new System.Drawing.Point(137, 312);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(100, 32);
+            this.btnGiris.TabIndex = 5;
+            this.btnGiris.Text = "Giriş Yap";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.BtnGiris_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(137, 282);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Beni Hatırla";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // lnkReSendPass
+            // 
+            this.lnkReSendPass.AutoSize = true;
+            this.lnkReSendPass.LinkColor = System.Drawing.Color.Red;
+            this.lnkReSendPass.Location = new System.Drawing.Point(156, 356);
+            this.lnkReSendPass.Name = "lnkReSendPass";
+            this.lnkReSendPass.Size = new System.Drawing.Size(81, 13);
+            this.lnkReSendPass.TabIndex = 7;
+            this.lnkReSendPass.TabStop = true;
+            this.lnkReSendPass.Text = "Şifremi Unuttum";
+            // 
+            // lnkNewAcc
+            // 
+            this.lnkNewAcc.AutoSize = true;
+            this.lnkNewAcc.Location = new System.Drawing.Point(93, 422);
+            this.lnkNewAcc.Name = "lnkNewAcc";
+            this.lnkNewAcc.Size = new System.Drawing.Size(98, 13);
+            this.lnkNewAcc.TabIndex = 8;
+            this.lnkNewAcc.TabStop = true;
+            this.lnkNewAcc.Text = "Yeni Hesap Oluştur";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 444);
+            this.Controls.Add(this.lnkNewAcc);
+            this.Controls.Add(this.lnkReSendPass);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.lblSifre);
+            this.Controls.Add(this.lblKullaniciAdi);
+            this.Controls.Add(this.pcbAvatar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAvatar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbAvatar;
+        private System.Windows.Forms.Label lblKullaniciAdi;
+        private System.Windows.Forms.Label lblSifre;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.LinkLabel lnkReSendPass;
+        private System.Windows.Forms.LinkLabel lnkNewAcc;
+    }
+}
+
---
+++ Degiskenler/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Degiskenler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void BtnGiris_Click(object sender, EventArgs e)
+        {
+            string kAdi, kSifre;
+            kAdi = txtKullaniciAdi.Text;
+            kSifre = txtSifre.Text;
+
+            if (string.IsNullOrEmpty(kAdi) && string.IsNullOrEmpty(kSifre))
+            {
+                MessageBox.Show("Kullanıcı Adı veya Şifre alanını doldurunuz !");
+            }
+            else if (kAdi != null && kSifre != null)
+            {
+                MessageBox.Show("Kullanıcı Adı : " + kAdi + " Şifre :" + kSifre );
+            }
+
+        }
+
+
+    }
+}
